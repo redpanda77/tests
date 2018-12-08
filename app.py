b65cba645276a010a56bd29943eaf59cfc27598c
redpanda77 <gonzalocasajus@gmail.com>
2018-12-08 22:12:34 +0100

    templates

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
+API_KEY = '61465cb89fcacaf8f3988c1b4296fe2b'
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def hello_world(name=None):
+    return render_template('layout.html', name=name)
